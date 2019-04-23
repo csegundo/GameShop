2bcb9ddaba1c9d87e886cbd7a31171126145473d
Carlos Segundo <45834017+csegundo@users.noreply.github.com>
2019-04-23 19:58:59 +0200

    SQL tables

---
+++ GameShopTables.sql
@@ -0,0 +1,44 @@
+CREATE TABLE `empleado` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `NIF` varchar(9) NOT NULL,
+  `turno` varchar(10) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `plataforma` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `producto` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `PVP` float NOT NULL,
+  `descripcion` varchar(150) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `tipo` varchar(45) NOT NULL,
+  `genero` varchar(45) NOT NULL,
+  `IDProveedor` int(11) NOT NULL,
+  `IDPlataforma` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `IDPlataforma_idx` (`IDPlataforma`),
+  KEY `IDProveedor_idx` (`IDProveedor`),
+  CONSTRAINT `IDPlataforma` FOREIGN KEY (`IDPlataforma`) REFERENCES `plataforma` (`ID`),
+  CONSTRAINT `IDProveedor` FOREIGN KEY (`IDProveedor`) REFERENCES `proveedor` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `proveedor` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `direccion` varchar(50) NOT NULL,
+  `NIF` varchar(9) NOT NULL,
+  `telefono` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `ticket` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `precio_final` float NOT NULL,
+  `fecha` date NOT NULL,
+  `IDEmpleado` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `IDEmpleado_idx` (`IDEmpleado`),
+  CONSTRAINT `IDEmpleado` FOREIGN KEY (`IDEmpleado`) REFERENCES `empleado` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
