e5701f1228327949ae783892bf27962b3f8886c5
joalow <joasm.98@zoho.eu>
2019-04-25 13:32:16 +0200

    script/main

--- GameShopProyect/src/Main/Main.java
+++ GameShopProyect/src/Main/Main.java
@@ -3,23 +3,21 @@ package Main;
 import javax.swing.SwingUtilities;
 
 import Presentacion.Controller.Controller;
+import Presentacion.View.InfoDB;
 
 public class Main {
 	
-	//No olvidar meter el campo activo a las entidades en la BD 
-	
-	
 	public static final String applicationName = "GameShop";
-	public static String database = "GameShop";
-	public static String user = "root";
-	public static String password = "gameshop";
+	public static String database;
+	public static String user;
+	public static String password;
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
 				Controller.getInstance();
-				//new InfoDB();
+				new InfoDB();
 			}
 		});
 	}
