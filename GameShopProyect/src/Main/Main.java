9c186502b1155a15aeed880e8a8395d16aa9e0c3
joalow <joasm.98@zoho.eu>
2019-04-29 15:12:08 +0200

    INFO DB:TO-DO:crear en una hebra y esperar hasta que se cierre

--- GameShopProyect/src/Main/Main.java
+++ GameShopProyect/src/Main/Main.java
@@ -3,14 +3,14 @@ package Main;
 import javax.swing.SwingUtilities;
 
 import Presentacion.Controller.Controller;
-//import Presentacion.View.InfoDB;
+import Presentacion.View.InfoDB;
 
 public class Main {
 	
 	public static final String applicationName = "GameShop";
 	public static String database = "gameshop";
 	public static String user = "root";
-	public static String password = "1234";
+	public static String password ;
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
