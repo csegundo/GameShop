35543052b16f150c1da9b51ad8d7d64e8dd7b1cd
Carlos Segundo <45834017+csegundo@users.noreply.github.com>
2019-05-01 01:04:13 +0200

    Subida manual de FormUpdateTicket
    Lo he subido a mano porque sino no me deja subirlo, no se por que puede ser.

---
+++ GameShopProyect/src/Presentacion/Ticket/FormUpdateTicket.java
@@ -0,0 +1,32 @@
+package Presentacion.Ticket;
+
+import java.awt.Component;
+import java.awt.Dimension;
+
+import javax.swing.JCheckBox;
+
+import Transfers.TTicket;
+
+@SuppressWarnings("serial")
+public class FormUpdateTicket extends FormTicket {
+	
+	private JCheckBox _reactivate = new JCheckBox("Activated");
+	private TTicket _tTicket;
+	
+	public FormUpdateTicket(TTicket tt) {
+		super();
+		this.setTitle("Modify a ticket");
+		this.setSize(new Dimension(320, 300));
+		initCheckBox();
+		_tTicket = tt;
+	}
+	
+	private void initCheckBox() {
+		_reactivate.setPreferredSize(new Dimension(75, 20));
+		_reactivate.setMinimumSize(new Dimension(75, 20));
+		_reactivate.setMaximumSize(new Dimension(75, 20));
+		_reactivate.setAlignmentX(Component.CENTER_ALIGNMENT);
+		_reactivate.setVisible(true);
+		this.add(_reactivate);
+	}
+}
