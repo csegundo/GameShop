192e586ba9637c80bb67110b6dc5d9c22c2e5787
Carlos Segundo <45834017+csegundo@users.noreply.github.com>
2019-04-30 17:57:17 +0200

    Add files via upload

---
+++ GameShopProyect/resources/GameShopTables.sql
@@ -0,0 +1,56 @@
+
+CREATE TABLE `empleado` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `NIF` varchar(9) NOT NULL,
+  `turno` varchar(10) NOT NULL,
+  `activo` tinyint(1) NOT NULL,
+
+  PRIMARY KEY (`ID`)
+);
+
+CREATE TABLE `plataforma` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `activo` tinyint(1) NOT NULL,
+  PRIMARY KEY (`ID`)
+);
+
+
+CREATE TABLE `proveedor` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `direccion` varchar(50) NOT NULL,
+  `NIF` varchar(9) NOT NULL,
+  `telefono` int(11) NOT NULL,
+  `activo` tinyint(1) NOT NULL,
+  PRIMARY KEY (`ID`)
+);
+
+CREATE TABLE `producto` (
+	`ID` int(11) NOT NULL AUTO_INCREMENT,
+	`nombre` varchar(45) NOT NULL,
+	`PVP` float NOT NULL,
+	`descripcion` varchar(150) NOT NULL,
+	`stock` int(11) NOT NULL,
+	`tipo` varchar(45) NOT NULL,
+	`genero` varchar(45) NOT NULL,
+	`IDProveedor` int(11) NOT NULL,
+	`IDPlataforma` int(11) NOT NULL,
+	`activo` tinyint(1) NOT NULL,
+	PRIMARY KEY (`ID`),
+	KEY `IDPlataforma_idx` (`IDPlataforma`),
+	KEY `IDProveedor_idx` (`IDProveedor`),
+	CONSTRAINT `IDPlataforma` FOREIGN KEY (`IDPlataforma`) REFERENCES `plataforma` (`ID`),
+	CONSTRAINT `IDProveedor` FOREIGN KEY (`IDProveedor`) REFERENCES `proveedor` (`ID`)
+);
+
+
+CREATE TABLE `ticket` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `precio_final` float NOT NULL,
+  `fecha` date NOT NULL,
+  `IDEmpleado` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `IDEmpleado_idx` (`IDEmpleado`),
+  CONSTRAINT `IDEmpleado` FOREIGN KEY (`IDEmpleado`) REFERENCES `empleado` (`ID`)
+);
