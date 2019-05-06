7937965b757e91cd9dcb82f34a92710c3dd463a3
Carlos Segundo <45834017+csegundo@users.noreply.github.com>
2019-05-06 23:22:00 +0200

    SQL actualizado

--- GameShopProyect/resources/GameShopTables.sql
+++ GameShopProyect/resources/GameShopTables.sql
@@ -68,3 +68,19 @@ CREATE TABLE `ticket` (
   CONSTRAINT `IDEmpl` FOREIGN KEY (`IDEmpl`) REFERENCES `empleado` (`ID`)
 );
 */
+
+CREATE TABLE `asociado` (
+  `IDProducto` INT(11) NOT NULL,
+  `IDTicket` INT(11) NOT NULL,
+  `IDEmpleado` INT(11) NOT NULL,
+  `cantidad` INT(10) NOT NULL,
+  PRIMARY KEY (`IDProducto`, `IDTicket`, `IDEmpleado`),
+  
+  KEY `IDEmpleado_idx` (`IDEmpleado`),
+  KEY `IDTicket_idx` (`IDTicket`),
+  KEY `IDProducto_idx` (`IDProducto`),
+
+  CONSTRAINT `IDEmpleadoP` FOREIGN KEY (`IDEmpleado`) REFERENCES `empleado` (`ID`),
+  CONSTRAINT `IDTicketP` FOREIGN KEY (`IDTicket`) REFERENCES `ticket` (`ID`),
+  CONSTRAINT `IDProductoP` FOREIGN KEY (`IDProducto`) REFERENCES `producto` (`ID`)
+);
