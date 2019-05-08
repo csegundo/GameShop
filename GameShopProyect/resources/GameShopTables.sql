1e562915fd28cc434a9f8cc9bacd6b9c334988a4
Carlos Segundo <45834017+csegundo@users.noreply.github.com>
2019-05-08 05:33:12 +0200

    Test ticket

--- GameShopProyect/resources/GameShopTables.sql
+++ GameShopProyect/resources/GameShopTables.sql
@@ -89,19 +89,21 @@ INSERT INTO proveedor(ID,direccion,NIF,telefono,activo) values (4,'calle igual',
 
 ----------------------------- 	PLATFORM   ---------------------------------------
 
-INSERT INTO `gameshop`.`plataforma` (`ID`, `nombre`, `activo`) VALUES ('1', 'XBOX360', '0');
-INSERT INTO `gameshop`.`plataforma` (`ID`, `nombre`, `activo`) VALUES ('2', 'NINTENDO-SWITCH', '1');
-INSERT INTO `gameshop`.`plataforma` (`ID`, `nombre`, `activo`) VALUES ('3', 'ORACLE', '1');
+INSERT INTO `plataforma` (`ID`, `nombre`, `activo`) VALUES ('1', 'XBOX360', '0');
+INSERT INTO `plataforma` (`ID`, `nombre`, `activo`) VALUES ('2', 'NINTENDO-SWITCH', '1');
+INSERT INTO `plataforma` (`ID`, `nombre`, `activo`) VALUES ('3', 'ORACLE', '1');
 
 ----------------------------- 	PRODUCT   ---------------------------------------
 
-INSERT INTO `gameshop`.`producto` (`ID`, `nombre`, `descripcion`, `PVP`, `stock`, `IDProveedor`, `IDPlataforma`, `activo`, `unidadesProv`, `genero`, `tipo`) VALUES ('1', 'Fifa', 'descripcion', '2', '6', '1', '1', '1', '6', 'Horror', 'VideoGame');
+INSERT INTO `producto` (`ID`, `nombre`, `descripcion`, `PVP`, `stock`, `IDProveedor`, `IDPlataforma`, `activo`, `unidadesProv`, `genero`, `tipo`) VALUES ('1', 'Fifa', 'descripcion', '2', '6', '1', '1', '1', '6', 'Horror', 'VideoGame');
+INSERT INTO `producto` (`ID`, `nombre`, `descripcion`, `PVP`, `stock`, `IDProveedor`, `IDPlataforma`, `activo`, `unidadesProv`, `genero`, `tipo`)
+						VALUES ('2', 'FORTNITE', 'descripcion', '20', '6', '1', '2', '1', '6', 'Shooter', 'VideoGame');
 
 ----------------------------- 	EMPLOYEE   ---------------------------------------
 
 
-INSERT INTO `gameshop`.`empleado` (`ID`, `nombre`, `NIF`, `turno`, `activo`) VALUES ('1', 'joaquin', '76664094B', 'Early shift', '1');
-INSERT INTO `gameshop`.`empleado` (`ID`, `nombre`, `NIF`, `turno`, `activo`) VALUES ('2', 'pepe', '94311944F','Early shift', '0');
-INSERT INTO `gameshop`.`empleado` (`ID`, `nombre`, `NIF`, `turno`, `activo`) VALUES ('3', 'pablo', '74769725Z','Early shift', '1');
+INSERT INTO `empleado` (`ID`, `nombre`, `NIF`, `turno`, `activo`) VALUES ('1', 'joaquin', '76664094B', 'Early shift', '1');
+INSERT INTO `empleado` (`ID`, `nombre`, `NIF`, `turno`, `activo`) VALUES ('2', 'pepe', '94311944F','Early shift', '0');
+INSERT INTO `empleado` (`ID`, `nombre`, `NIF`, `turno`, `activo`) VALUES ('3', 'pablo', '74769725Z','Early shift', '1');
 
 
--- GameShopProyect/test/Negocio/Ticket/TestSATicket.java
+++ GameShopProyect/test/Negocio/Ticket/TestSATicket.java
@@ -1,85 +1,97 @@
-
 package Negocio.Ticket;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import Integracion.DAO.DAOAbstractFactory;
 import Integracion.Ticket.DAOTicket;
+import Negocio.SA.SAAbstractFactory;
+import Transfers.TGame;
+import Transfers.TPlatform;
 import Transfers.TProduct;
 import Transfers.TTicket;
 
 
-public class TestSATicket i{
+public class TestSATicket {
 	
-	public Integer createTicket(TTicket tt) {
-		//DAOTicket daoTicket = DAOAbstractFactory.getInstance().createDAOTicket();
-		if(prodListNotEmpty(tt) && correctInputData(tt)){
-			// code ==> Calcular precio final y modificar el stock de los productos en la BD de producto
-			return DAOAbstractFactory.getInstance().createDAOTicket().createTicket(tt); 
-		}
-		else
-		{
-			return 0;
-		}
-	}
-
-	public Boolean deleteTicket(TTicket tt) {
-		boolean deleted=false;
-		DAOTicket daoTicket = DAOAbstractFactory.getInstance().createDAOTicket();
-		if(tt!=null)
-		{
-			TTicket aux = daoTicket.readTicket(tt);
-			if(aux!=null)
-				deleted= daoTicket.deleteTicket(tt);
-		}
-		return deleted;
-	}
-
-	public Boolean updateTicket(TTicket tt) { //TODO ?????????????????????????????????????
-		return null;
-		// end-user-code
-	}
-	public Object readTicket(Integer id) {
-		TTicket ret = null;
-		DAOTicket daoTicket = DAOAbstractFactory.getInstance().createDAOTicket();
-		if(id != null)
-		{
-			TTicket aux = new TTicket();
-			aux.set_id(id);
-			ret = (TTicket)daoTicket.readTicket(aux);
-		}
-		return ret;
+	private SATicket st = SAAbstractFactory.getInstance().createSATicket();
+	
+	@Test
+	public void testCreateTicketOk() {
+		// validez sintactica,nombre no existente
+		//TPlatform tp = new TPlatform("PS3");
+		//assertEquals(4,sa.createPlatform(tp).intValue());
+		ArrayList<Object> p = new ArrayList<Object>();
+		p.add(new TGame("FORTNITE", 6, 20.0, 1, 2, "descripcion", "Shooter"));
+		TTicket tt = new TTicket(1, p);
+		assertEquals();
 	}
 	
-	public List<Object> readAllTickets() {
-		List<Object> tickets = null;
-		tickets = DAOAbstractFactory.getInstance().createDAOTicket().readAllTickets();
-		return tickets;
-	}
-	
-	private boolean prodListNotEmpty(TTicket tt)
-	{ //damos por hecho en el DAO que la lista no llega vacia ===> Aqui se comprueba si esta vacia o no
-		return tt.get_products().size()>0;
-	}
-	
-	private boolean correctInputData(TTicket tt)
-	{ //comprobamos que todos los datos introducidos son correctos y existen
-	
-		//Empleado existe y esta activo
-		if(DAOAbstractFactory.getInstance().createDAOEmployee().readEmployee(tt.get_employeeId())==null)
-			return false;
+	@Test
+	public void testCreateTicketFail() {
+		// invalidez sintactica
+		//TPlatform tp = new TPlatform("");	//nombre vacio
+		//assertNotEquals(5,sa.createPlatform(tp).intValue());
+		//tp = new TPlatform("PS3PS3PS3PS3PS3PS3PS3PS3PS3PS3PS3PS3PS3PS3PS3PS3");//nombre >45 chars
+		//assertNotEquals(5,sa.createPlatform(tp).intValue());
+		// nombre existe
+		//tp = new TPlatform("XBOX360");
+		//assertNotEquals(5,sa.createPlatform(tp).intValue());
 		
-		//Cada uno de los productos existen y hay stock suficiente
-		int prodAmount = tt.get_products().size();
-		for(int i=0;i<prodAmount;i++)
-		{
-			TProduct prod = (TProduct) tt.get_products().get(i);
-			TProduct aux = DAOAbstractFactory.getInstance().createDAOProduct().readProduct(prod.get_id());
-			if(aux==null)
-				return false;
-			if((aux.get_stock() - prod.get_unitsProvided()<0))
-				return false;
-		}
-		return true;
+		//assertNotEquals(5,sa.createPlatform(null).intValue());
 	}
+
+	@Test
+	public void testUpdateTicketOk() {
+		//Plataforma existe,validez sintactica(NOMBRE A CAMBIAR NO EXISTE)
+		//TPlatform tp = new TPlatform("STEAM");
+		//tp.set_id(3);
+		//tp.set_activated(true);
+		//assertTrue(sa.updatePlatform(tp));
+	}
+	
+	@Test
+	public void testUpdateTicketFail() {
+		//TPlatform tp = new TPlatform();
+		//invalidez sintactica
+		//assertFalse(sa.updatePlatform(tp));	//campos nulos
+		//tp.set_name("");
+		//assertFalse(sa.updatePlatform(tp)); //name empty y campos nulos
+		//tp.set_name("PS3PS3PS3PS3PS3PS3PS3PS3PS3PS3PS3PS3PS3PS3PS3PS3");
+		//assertFalse(sa.updatePlatform(tp)); //nombre >45 chars y campos nulos
+		//tp.set_name("STEAM"); //campos nulos menos nombre
+		//assertFalse(sa.updatePlatform(tp)); //name empty y campos nulos
+		//tp.set_activated(true);
+		//assertFalse(sa.updatePlatform(tp)); //id nulo
+		//Plataforma no existe
+		//tp.set_id(8);
+		//assertFalse(sa.updatePlatform(tp));
+	}
+	
+	@Test
+	public void testReadTicketOk() {
+		//Plataforma existe
+		//TPlatform tp = new TPlatform("XBOX360");
+		//tp.set_id(1);
+		//tp.set_activated(false);
+		//assertEquals(tp.toString(),((TPlatform)sa.readPlatform(1)).toString());
+	}
+	
+	@Test
+	public void testReadTicketFail() {
+		//Plataforma no existe
+		//TPlatform tp = new TPlatform("XBOX360");
+		//tp.set_id(8);
+		//tp.set_activated(false);
+		//assertNotEquals(tp,((TPlatform)sa.readPlatform(8)));
+		//assertNotEquals(tp,sa.readPlatform(null));
+	}
+	
 }
